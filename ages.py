306304102cacbbb4ff35f8f61990e7f7a85a607f
sungcc <samooies@yahoo.com.hk>
2023-01-03 11:12:54 +0800

    try function

---
+++ ages.py
@@ -0,0 +1,21 @@
+while True:
+    print ("Enter your age:")
+
+    age = input()
+
+    try:
+        age = int(age)
+    except:
+        print ("Please use numeric digits")
+
+        continue
+
+    if age < 1 or age > 120:
+        print ("Please enter a positive number")
+
+        continue
+
+
+    break
+
+print(f"Your age is {age}.")
